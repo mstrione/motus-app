b6f502173ec06e004c05b2cb2795dee14fa82e01
mstrione <mauro.strione@gmail.com>
2020-01-29 23:30:46 -0300

    Agrego rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
