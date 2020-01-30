4a00ddaf6a4ed9d839b69032d1bffe1550d3c137
mstrione <mauro.strione@gmail.com>
2020-01-29 23:22:03 -0300

    Fix de error de sintaxis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,9 +16,6 @@ export const environment = {
 
 };
 
-var 
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
