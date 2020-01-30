009f1d531c7d12e264950fee2428cad651614a26
mstrione <mauro.strione@gmail.com>
2020-01-30 00:22:22 -0300

    Creación de usuario y guardado de datos en DB Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     AngularFireModule.initializeApp( environment.firebaseConfig ),
     AngularFireAuthModule,
     AngularFirestoreModule
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private eventAuthError = new BehaviorSubject<string>('');
+  eventAuthError$ = this.eventAuthError.asObservable();
+  newUser: any;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {}
+
+  createUser(user) {
+    this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password)
+    .then (userCredential => {
+      this.newUser = user;
+      userCredential.user.updateProfile({
+        displayName: user.firsname + ' ' + user.lastname
+      });
+      this.insertUserData(userCredential)
+      .then( () => {
+        this.router.navigate(['/home']);
+      });
+    })
+    .catch(error => {
+      this.eventAuthError.next(error);
+    });
+  }
+
+  insertUserData(userCredential: firebase.auth.UserCredential) {
+    return this.db.doc(`Users/${userCredential.user.uid}`).set({
+          email: this.newUser.email,
+          fistName: this.newUser.firstName,
+          lastName: this.newUser.lastName,
+          role: 'USUARIO WEB'
+        }
+    );
+  }
 }
+
